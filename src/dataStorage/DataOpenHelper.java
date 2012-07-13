a68b6479216284550812381443da2f2125b3aea0
Dim <Dim@192.168.1.2>
2012-07-13 16:09:24 +0200

    Suite de la classe DataOpenHelper

--- src/dataStorage/DataOpenHelper.java
+++ src/dataStorage/DataOpenHelper.java
@@ -1,6 +1,7 @@
 package dataStorage;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -10,7 +11,7 @@ public class DataOpenHelper extends SQLiteOpenHelper
 {
 	
 	/**
-	 *    Constantes
+	 *    CONSTANTES
 	 *
 	 */
 	
@@ -26,13 +27,39 @@ public class DataOpenHelper extends SQLiteOpenHelper
 	// Description des colonnes
 	
 	
-	// Requête SQL pour la création da la base
+	// Requête SQL pour la création de la base
+	private static final String REQUETE_CREATION_BDD = "CREATE TABLE ";
 	
 	
 	// Constructeur
 	public DataOpenHelper(Context context, String name, CursorFactory factory, int version) 
 	{
-		super(context, name, factory, version);
+		super(context, FIRST_BASE_NAME, factory, DATABASE_VERSION);
+	}
+	
+			 
+	/**
+	 * Création de la base
+	 */
+	@Override	
+	public void onCreate(SQLiteDatabase db) 
+	{
+		    db.execSQL(REQUETE_CREATION_BDD);
+	}
+			 
+	/**
+	 * Mise à jour de la base
+	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+	    // Lorsque l'on change le numéro de version de la base on supprime la
+	    // table puis on la recrée
+	    if (newVersion > DATABASE_VERSION) 
+	    {
+	    	db.execSQL("DROP TABLE " + FIRST_TABLE_NAME + ";");
+	    	onCreate(db);
+	    }
 	}
 	
 }
