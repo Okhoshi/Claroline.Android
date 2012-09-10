d4e8f5b0df442b0b588bbd68d82ecc31b08f801c
Dim <Dim@192.168.1.4>
2012-09-11 00:27:45 +0200

    Tentative de résolution des problèmes concernant les TabHost

--- src/activity/ListeDocument.java
+++ src/activity/ListeDocument.java
@@ -38,71 +38,11 @@ public class ListeDocument extends Activity
 		List<Documents> Liste = currentCours.getDocuments();
 		DocumentsAdapter adapter = new DocumentsAdapter(this, layoutID3, Liste);
 		list.setAdapter(adapter);
-		
-		setActionBar();
-		setOverflowMenu();
-		
+
 	}
 	
 	
 	
-	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.menu_about:
-            // Comportement du bouton "A Propos"
-        	Intent monIntent = new Intent(this,about_us.class);
-        	startActivity(monIntent);
-            return true;
-        case R.id.menu_help:
-            // Comportement du bouton "Aide"
-            return true;
-        case R.id.menu_refresh:
-            // Comportement du bouton "Rafraichir"
-            return true;
-        case R.id.menu_search:
-            // Comportement du bouton "Recherche"
-        	Intent monIntent1 = new Intent(this,searchableActivity.class);
-        	startActivity(monIntent1);
-        	//onSearchRequested();
-            return true;
-        case R.id.menu_settings:
-        	Intent settings_intent = new Intent(this, Settings.class);
-        	startActivity(settings_intent);
-        	return true;
-        case android.R.id.home:
-        	// Comportement du bouton qui permet de retourner a l'activite precedente
-        	monIntent = new Intent(this,home.class);
-        	startActivity(monIntent);
-        	return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
-	
-	
-	// Met les propriétés de l'action bar
-		public void setActionBar()
-		{
-			ActionBar actionBar = getActionBar();
-	        actionBar.setDisplayHomeAsUpEnabled(true); 
-		}
-		
-		
-		public void setOverflowMenu()
-	    {
-	    	try {
-	            ViewConfiguration config = ViewConfiguration.get(this);
-	            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
-	            if(menuKeyField != null) {
-	                menuKeyField.setAccessible(true);
-	                menuKeyField.setBoolean(config, false);
-	            }
-	        } catch (Exception ex) {
-	            // Ignore
-	        }
-
-	    }
 		
 		
 		public void onListItemClick(ListView l, View v, int position, long id) {
