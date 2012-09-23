4fb906d04cecfb00824c463c8030a5e5d70e554a
qdevos <qdevos@git>
2012-09-23 18:40:38 +0200

    [Work in Progress] Reecriture des fragments

--- src/fragments/mainCoursFragment.java
+++ src/fragments/mainCoursFragment.java
@@ -39,20 +39,8 @@ public class mainCoursFragment extends ListFragment
 		super.onActivityCreated(savedInstanceState);
 		int layoutID = R.layout.cours_view;
 		
-		// Normalement ca ! -->
 		List<Cours> Liste = CoursRepository.GetAllCours();
-		// Test avec ca 
-
-		/*
-		final List<Cours> Liste= new ArrayList<Cours>();
-		Cours Cours1= new Cours(null, null, null, null,"ives.smeers@uclouvain.be" , null, "Economie d'entreprise", "Ives Smeers");
-		Cours Cours2= new Cours(null, null, null, null, "peter.vanroy@uclouvain.be", null, "Informatique : Oz", "Peter Van Roy");
-		Cours Cours3= new Cours(null, null, null, null, "francois.remacle@uclouvain.be", null, "Mathématique Q3", "Francois Remacle");
-		Liste.add(Cours1);
-		Liste.add(Cours2);
-		Liste.add(Cours3);*/
-		CoursAdapter adapter = new CoursAdapter(getActivity().getApplicationContext(),
-				layoutID, Liste);
+		CoursAdapter adapter = new CoursAdapter(getActivity(),layoutID, Liste);
 		setListAdapter(adapter);
 		registerForContextMenu(getListView());
 	}
@@ -61,8 +49,7 @@ public class mainCoursFragment extends ListFragment
 		public void handleMessage(Message mess){
 			int layoutID = R.layout.cours_view;
 			List<Cours> Liste = CoursRepository.GetAllCours();
-			CoursAdapter adapter = new CoursAdapter(getActivity().getApplicationContext(),
-					layoutID, Liste);
+			CoursAdapter adapter = new CoursAdapter(getActivity(),layoutID, Liste);
 			setListAdapter(adapter);
 		}
 	};
@@ -75,50 +62,13 @@ public class mainCoursFragment extends ListFragment
 	 * 
 	 */
 	
-	
-	
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Cours item = (Cours) getListAdapter().getItem(position);
-		home.currentCours=item;
-		String title = item.getTitle();
-		String titular = item.getTitular();
 		
-		detailsAnnonceCoursFragment fragmentAnnonce = (detailsAnnonceCoursFragment) getFragmentManager().findFragmentById(R.id.details_frag);
-		detailsDocumentsCoursFragment fragmentDocs = (detailsDocumentsCoursFragment) getFragmentManager().findFragmentById(R.id.details_frag_docs);
-		
-		//Log.v("MO", home.currentTag);
-		
-		if (fragmentAnnonce != null && fragmentAnnonce.isInLayout() && home.currentTag.equals(home.ANNONCE_ID)) 
-		{
-			//fragmentAnnonce.setText(title);
-			int layoutID2 = R.layout.details_annonce;			
-			// Normalement ca ! -->
-			List<Annonce> Liste = item.getAnnonces();
-			
-			AnnonceAdapter adapter = new AnnonceAdapter(getActivity().getApplicationContext(),
-					layoutID2, Liste);
-			setListAdapter(adapter);
-			registerForContextMenu(getListView());
-		} 
-		if (fragmentDocs != null && fragmentDocs.isInLayout() && home.currentTag.equals(home.DOCUMENTS_ID))
-		{
-			//fragmentAnnonce.setText(titular);
-			int layoutID4 = R.layout.details_annonce;		//TODO changer ici	
-			List<Documents> Liste = item.getDocuments();
-			
-			DocumentsAdapter adapter = new DocumentsAdapter(getActivity().getApplicationContext(),
-					layoutID4, Liste);
-			setListAdapter(adapter);
-			registerForContextMenu(getListView());
-		} 
-		else 
-		{
-			Intent intent = new Intent(getActivity().getApplicationContext(), activity.coursActivity.class);
-			intent.putExtra("coursID", item.getId());
-			startActivity(intent);
-		}
-
+		Intent intent = new Intent(getActivity().getApplicationContext(), activity.coursActivity.class);
+		intent.putExtra("coursID", item.getId());
+		startActivity(intent);
 	}
 	
 	
@@ -129,18 +79,16 @@ public class mainCoursFragment extends ListFragment
 	 * 
 	 */
 	
-	
-	
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		menu.setHeaderTitle(getString(R.string.contextual_header));
 		menu.add(0, MAIL_ID, 0, getString(R.string.contextual_mail));
-}
+	}
 
-public boolean onContextItemSelected(MenuItem item) {
-	AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-	switch (item.getItemId()) 
-	{
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		switch (item.getItemId()) 
+		{
 		case MAIL_ID:
 			//String officialEmail = currentCours.getOfficialEmail();
 			// Pour test
@@ -150,10 +98,10 @@ public boolean onContextItemSelected(MenuItem item) {
 			i.putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_titulars));
 			i.setType("message/rfc822");
 			startActivity(Intent.createChooser(i, getString(R.string.choose_activity_mail)));
-		return true;
+			return true;
 		default:
-		return super.onContextItemSelected(item);
+			return super.onContextItemSelected(item);
+		}
+
 	}
-	
-}
 }
