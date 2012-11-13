b074bef7c80a12a0c3eb588019bd146b02da2a3e
Dim <Dim@192.168.1.2>
2012-11-13 18:15:38 +0100

    Affichage du nom de la plateforme, nom de l'institution, du nom et prénom et du NOMA. NOMA ne s'affiche , peut-être un problème...

--- src/fragments/contextFragment.java
+++ src/fragments/contextFragment.java
@@ -13,20 +13,34 @@ import app.GlobalApplication;
 
 public class contextFragment extends Fragment implements OnSharedPreferenceChangeListener {
 
-	private static final String AUTH_PLATFORM_TEXT = "platformTextAuth";
-	private static final String ANON_PLATFORM_TEXT = "platformTextAnonym";
-	private TextView authView;
-	private TextView anonView;
+	//private static final String AUTH_PLATFORM_TEXT = "platformTextAuth";
+	//private static final String ANON_PLATFORM_TEXT = "platformTextAnonym";
+	
+	private static final String PLATFORM_NAME = "platformName";
+	private static final String INSTITUTION_NAME = "institutionName";
+	private static final String FIRST_NAME = "firstName";
+	private static final String LAST_NAME = "lastName";
+	private static final String NOMA = "officialCode";
+
+	private TextView platformView;
+	private TextView institutionView;
+	private TextView firstLastView;
+	private TextView nomaView;
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View view = inflater.inflate(R.layout.context_frag, container, false);
 
-		anonView = (TextView) view.findViewById(R.id.anonymous_platform_text);
-		authView = (TextView) view.findViewById(R.id.auth_platform_text);
+		platformView = (TextView) view.findViewById(R.id.platformName);
+		institutionView = (TextView) view.findViewById(R.id.institutionName);
+		firstLastView = (TextView) view.findViewById(R.id.firstLastName);
+		nomaView = (TextView) view.findViewById(R.id.NOMA);
 		
-		authView.setText(GlobalApplication.getPreferences().getString(AUTH_PLATFORM_TEXT,""));
-		anonView.setText(GlobalApplication.getPreferences().getString(ANON_PLATFORM_TEXT, ""));
+		platformView.setText(GlobalApplication.getPreferences().getString(PLATFORM_NAME,""));
+		institutionView.setText(GlobalApplication.getPreferences().getString(INSTITUTION_NAME,""));
+		firstLastView.setText(GlobalApplication.getPreferences().getString(FIRST_NAME,"")+" "+GlobalApplication.getPreferences().getString(LAST_NAME,""));	
+		nomaView.setText(GlobalApplication.getPreferences().getString(NOMA,""));
 		
 		return view;
 	}
@@ -44,10 +58,14 @@ public class contextFragment extends Fragment implements OnSharedPreferenceChang
 	}
 	
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreference, String key) {
-		if(key.equals(AUTH_PLATFORM_TEXT)){
-			authView.setText(GlobalApplication.getPreferences().getString(key,""));
-		} else if(key.equals(ANON_PLATFORM_TEXT)){
-			anonView.setText(GlobalApplication.getPreferences().getString(key, ""));
+		if(key.equals(FIRST_NAME)||key.equals(LAST_NAME)){
+			firstLastView.setText(GlobalApplication.getPreferences().getString(key,""));
+		} else if(key.equals(PLATFORM_NAME)){
+			platformView.setText(GlobalApplication.getPreferences().getString(key, ""));
+		} else if(key.equals(NOMA)){
+			nomaView.setText(GlobalApplication.getPreferences().getString(key, ""));
+		}else if(key.equals(NOMA)){
+			institutionView.setText(GlobalApplication.getPreferences().getString(key, ""));
 		}
 	}
 }
